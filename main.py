2c53f38b92e5638389c41ed1d32c282f5f7e11cb
shashankdixit98@gmail.com <shashankdixit98@gmail.com>
2023-06-18 18:17:39 +0200

    feat(api): adding fastapi in main

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from gpt_backend.logger import Logger
 from gpt_backend.Assistant import Assistant
 from datetime import datetime
+from fastapi import FastAPI
 import json
 
-if __name__ == "__main__":
+
+app = FastAPI()
+
+user_msg = f" I am a taekwondo professional who stopped training for two years." \
+           f" Give me a workout schedule to get back into shape in a month's time period." \
+           f" in a JSON format."
 
 
-    user_msg = f" I am a taekwondo professional who stopped training for two years." \
-               f" Give me a workout schedule to get back into shape in a month's time period." \
-               f" in a JSON format."
 
+@app.get("/",operation_id="getState",description="Index page",name="get intro page",tags=["intro"])
+def index():
+    return{"Info":"Welcome to Training Bot"}
+
+
+@app.post("/first_request",operation_id="firstRequest",tags=["request"])
+async def first_req():
     Trainer1 = Assistant("taekwondo", "professional", 21, 3)
     response = Trainer1.create_prompt_message(user_msg)
-
     # Log the Input and Output for study
     time_stamp = datetime.now()
     dt_string = time_stamp.strftime("%d/%m/%Y %H:%M:%S")
     log = Logger(user_msg, [Trainer1.model, Trainer1.sport, Trainer1.user_experience, Trainer1.bmi, Trainer1.workout_intensity], response, dt_string)
     log.log_data()
-
     data = json.loads(response)
-    print(data)
+    return data
